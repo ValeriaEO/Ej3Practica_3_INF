223ddaa948078c0c39e2077d925c6cccbcdf3f3c
ValeriaEO <113623864+ValeriaEO@users.noreply.github.com>
2022-09-29 18:27:14 +0200

    Update dec2bin.py

--- dec2bin.py
+++ dec2bin.py
@@ -8,7 +8,7 @@ def dec2bin(numero_decimal, numero_bits):
         
             numero_binario = "0" + numero_binario
     else:
-        numero_binario = numero_binario[3:len(numero_binario)]
+        numero_binario = numero_binario[2:len(numero_binario)]
         while len(numero_binario) < numero_bits:
             numero_binario = "1" + numero_binario
     return numero_binario
